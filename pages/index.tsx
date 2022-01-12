f1fde92d353814275a64bc4eced6e8fde43fd732
Jack Pee <mcinvasion2016@gmail.com>
2022-01-11 21:51:58 -0700

    feat: navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,39 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
-        <Image src="/images/logo.png" alt="logo" width={200} height={200} />
-        <div className={styles.text}>
-          <h1>MangaUpdates Bot</h1>
-          <h2>A nifty Discord bot that alerts you on new manga chapters.</h2>
+      <div className={styles.top}>
+        <div className={styles.title}>
+          <Image src="/images/logo.png" alt="logo" width={200} height={200} />
+          <div className={styles.text}>
+            <h1>MangaUpdates Bot</h1>
+            <h2>A nifty Discord bot that alerts you on new manga chapters.</h2>
+          </div>
+        </div>
+        <div className={styles.nav}>
+          <div className={styles.navItem}>
+            <a href="https://discord.gg/UcYspqftTF">
+              <Image src="/images/nav/nav1.png" alt="nav1" width={125} height={125} />
+              <h2>Support</h2>
+            </a>
+          </div>
+          <div className={styles.navItem}>
+            <a href="botdocs.haysaka.moe">
+              <Image src="/images/nav/nav2.png" alt="nav2" width={125} height={125} />
+              <h2>Docs</h2>
+            </a>
+          </div>
+          <div className={styles.navItem}>
+            <a href="https://discord.com/oauth2/authorize?client_id=880694914365685781&scope=applications.commands%20bot&permissions=268856384">
+              <Image src="/images/nav/nav3.png" alt="nav3" width={125} height={125} />
+              <h2>Invite</h2>
+            </a>
+          </div>
         </div>
       </div>
-      <div className={styles.nav}>
-        <a href="https://discord.gg/UcYspqftTF">Support</a>
+      <div className={styles.bottom}>
       </div>
     </div>
+    
   )
 }
 
